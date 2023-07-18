1c58654bed2a7cd7b2d84c21d9154272f014cb76
sogafish <kent.soga@gmail.com>
2023-07-18 22:42:23 +0900

    Add ItemPost Endpoint

--- src/app/api/item/route.ts
+++ src/app/api/item/route.ts
@@ -1,8 +1,32 @@
 import { NextResponse } from 'next/server'
 import { prismaClient } from '../lib/prisma';
+import { NextApiRequest } from 'next';
 
 export const GET = async (req: Request) => {
   const data = await prismaClient.item.findMany();
 
   return NextResponse.json({ status: 200, data});
+}
+
+export const POST = async (req: Request) => {
+  const { title, description } = await req.json();
+
+  try {
+    const data = await prismaClient.item.create({
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+      data: {
+        title,
+        description,
+      },
+    });
+    return NextResponse.json({ status: 200, data });
+  } catch (e) {
+    console.error(e);
+  }
 }
