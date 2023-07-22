a342fbf72a070fdd474987c3874170dea894f782
sogafish <kent.soga@gmail.com>
2023-07-22 13:29:10 +0900

    Add DELETE path

--- src/app/api/item/[id]/route.ts
+++ src/app/api/item/[id]/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server'
+import { prismaClient } from '../../lib/prisma';
 
 export const GET = async (req: Request, { params }: { params: { id: string } }) => {
   // const { searchParams } = new URL(req.url);
@@ -8,4 +9,20 @@ export const GET = async (req: Request, { params }: { params: { id: string } }) 
 export const PUT = async (req: Request, { params }: { params: { id: string } }) => {
   // const { searchParams } = new URL(req.url);
   return NextResponse.json({ status: 200, data: params });
+}
+
+export const DELETE = async (req: Request, { params }: { params: { id: string } }) => {
+  try {
+    const data = await prismaClient.item.delete({
+      select: {
+        id: true,
+      },
+      where: {
+        id: parseInt(params.id, 10),
+      },
+    });
+    return NextResponse.json({ status: 200, data });
+  } catch (e) {
+    console.error(e);
+  }
 }
