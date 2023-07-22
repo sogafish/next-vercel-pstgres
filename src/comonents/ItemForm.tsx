e4a966bedfbe02e403808a3b6520d76b6651d92e
sogafish <kent.soga@gmail.com>
2023-07-22 13:19:42 +0900

    Fix Form

--- src/comonents/ItemForm.tsx
+++ src/comonents/ItemForm.tsx
@@ -1,6 +1,7 @@
 import { Item } from '@prisma/client';
 import { useForm } from 'react-hook-form';
 import { METHODS } from 'http';
+import { css } from '../../styled-system/css';
 
 type Props = {
   successCallback: (v: Item) => void;
@@ -26,14 +27,45 @@ export const ItemForm = ({ successCallback }: Props) => {
     });
   };
 
-  console.log(watch("title"));
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input placeholder="title" {...register("title", { required: true })} />
-      {errors.title && <span>This field is required</span>}
-      <input placeholder="description" {...register("description")} />
-      <input type="submit" />
+    <form onSubmit={handleSubmit(onSubmit)} className={css({ flexDirection: 'column', display: 'flex' })}>
+    <input className={titleInputStyle} placeholder="title" {...register("title", { required: true })} />
+    {errors.title ?
+      <span className={css({ fontSize: '12px', color: 'red.400' })}>This field is required</span>
+      : <span className={css({ height: 4 })} />
+    }
+    <textarea className={descriptionInputStyle} placeholder="description" {...register("description")} />
+    <button type="submit" className={buttonStyle}>SUBMIT</button>
     </form>
   );
 };
+
+const titleInputStyle = css({
+  borderWidth: '1px',
+  borderColor: 'gray.300',
+  borderRadius: 4,
+  padding: 1,
+});
+
+const descriptionInputStyle = css({
+  mt: 4,
+  borderWidth: '1px',
+  borderColor: 'gray.300',
+  borderRadius: 4,
+  padding: 1,
+});
+const buttonStyle = css({
+  mt: 4,
+  borderWidth: '1px',
+  borderColor: 'gray.300',
+  borderRadius: 4,
+  display: 'inline-block',
+  padding: 1,
+  width: 'auto',
+  background: 'BEAVER',
+  cursor: 'pointer',
+  color: 'white',
+  _hover: {
+    background: 'PALE_DOGWOOD'
+  },
+});
