964985ac4b6c927c81e69f210645dfd2702e98a3
sogafish <kent.soga@gmail.com>
2023-09-10 22:14:32 +0900

    Fix Canvas

--- src/app/canvas/canvasContainer.tsx
+++ src/app/canvas/canvasContainer.tsx
@@ -1,11 +1,40 @@
 "use client"
-import { useCallback, useEffect, useState } from 'react';
+import { MutableRefObject, useCallback, useEffect, useRef, useState } from 'react';
 import { CanvasPresenter } from './canvasPresenter';
 
 export const CanvasContainer = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [text, setText] = useState<string>('');
+
+  const getCanvasContext = useCallback(() => {
+    if (!canvasRef?.current) return null;
+
+    return canvasRef.current.getContext('2d');
+  }, []);
+
+  // Initial
+  useEffect(() => {
+    // const canvasElem = document.querySelector('canvas');
+    // if (!canvasElem) return;
+  }, []);
+
+  // Text
+  useEffect(() => {
+    const ctx = getCanvasContext();
+    if (!ctx) return;
+    ctx.fillText(text, 8, 8);
+    ctx.save();
+  }, [getCanvasContext, text]);
+  const onChangeText = useCallback((v: string) => {
+    setText(v);
+  }, []);
+
   return (
     <div>
-      <CanvasPresenter />
+      <CanvasPresenter
+        canvasRef={canvasRef}
+        onChangeText={onChangeText}
+      />
     </div>
   )
 }
--- src/app/canvas/canvasPresenter.tsx
+++ src/app/canvas/canvasPresenter.tsx
@@ -1,12 +1,43 @@
 "use client"
+import { RefObject } from "react";
 import { css } from "../../../styled-system/css";
+import { flex } from "../../../styled-system/patterns";
 
-type Props = {};
+type Props = {
+  canvasRef: RefObject<HTMLCanvasElement>;
+  onChangeText: (v: string) => void;
+};
 
 export const CanvasPresenter = (props: Props) => {
+  const { canvasRef, onChangeText } = props;
+
   return (
     <main>
-      <p>Canvas</p>
+      <div className={canvasContainer}>
+        <canvas
+          ref={canvasRef}
+          width={400}
+          height={280}
+          className={canvasStyle}
+        />
+      </div>
+      <form className={formStyle}>
+        <input
+          placeholder="Type something."
+          onChange={(event) => onChangeText(event.currentTarget?.value)}
+        />
+      </form>
     </main>
   )
 }
+
+const canvasContainer = flex({
+  display: 'flex',
+  justifyContent: 'center',
+});
+const canvasStyle = css({
+  border: '1px solid gray',
+});
+const formStyle = css({
+  mt: 4,
+});
